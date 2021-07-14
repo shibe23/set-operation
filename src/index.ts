d94103b250a5eff7c63c7a40408abd4d1a01f00b
shibe23 <shibe23.info@gmail.com>
2021-07-15 00:20:18 +0900

    chore: fix JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 /**
  * オブジェクトが格納された配列の差集合を求める
- * @returns Object[]
+ * @param {Array.<T>} base
+ * @param {Array.<T>} target
+ * @param {string} key
+ * @returns {Array.<T>}
  */
 export function difference<T>(base: T[], target: T[], key: string): T[] {
   const result: T[] = [];
@@ -20,7 +23,10 @@ export function difference<T>(base: T[], target: T[], key: string): T[] {
 
 /**
  * オブジェクトが格納された配列の積集合を求める
- * @returns Object[]
+ * @param {Array.<T>} base
+ * @param {Array.<T>} target
+ * @param {string} key
+ * @returns {Array.<T>}
  */
 export function intersection<T>(base: T[], target: T[], key: string): T[] {
   const result: T[] = [];
@@ -38,6 +44,12 @@ export function intersection<T>(base: T[], target: T[], key: string): T[] {
   return result;
 }
 
+/**
+ * オブジェクトから指定したvalueを抽出し、keyに変換する
+ * @param {Array.<T>} base
+ * @param {string} key
+ * @returns {Object.<string, unknown>>}
+ */
 function extractKeys<T>(base: T[], key: string) {
   const lookupRecord: Record<string, unknown> = {};
   base.forEach((x: Record<string, any>) => {
